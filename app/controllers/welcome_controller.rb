cf1498858c8eed56fe355b4f165a38e60183623f
Dominic Ho <domtkho@gmail.com>
2014-10-16 15:53:03 +0800

    Added database table and skeleton for recommendation section

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+    @deliver_time = Time.now + 45.minutes
+    @deliver_time = @deliver_time.strftime "%I: %M %P"
+    if params[:admin] == "true"
+      render 'index'
+    else
+      render 'under_constructions'
+    end
   end
 end
